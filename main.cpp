4a65f1c182c65b3255b413762aa14a4bfde4f489
ravenwijaya <58552967+ravenwijaya@users.noreply.github.com>
2020-07-13 20:55:10 +0700

    bangunan 50% animasi done

--- main.cpp
+++ main.cpp
@@ -20,14 +20,27 @@ float xdiff = 0.0f;
 float ydiff = 0.0f;
 bool mouseDown = false;
 int is_depth;
+ //Deklarasi Variable
+float z_pos=-10.0f;
+float rot=0.0f;
+
+ void myTimeOut(int id)
+{
+// called if timer event
+// ...advance the state of animation incrementally...
+       rot+=10;
+       glutPostRedisplay(); // request redisplay
+       glutTimerFunc(100, myTimeOut, 0); // request next timer event
+}
 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(800, 600);
+	glutInitWindowSize(1000, 600);
 	glutInitWindowPosition(100, 50);
 	glutCreateWindow("TR_GRAFKOM_J_6");
+	glutTimerFunc(100, myTimeOut, 0);
 	init();
 	glutDisplayFunc(tampil);
 	glutKeyboardFunc(keyboard);
@@ -35,7 +48,6 @@ int main(int argc, char** argv)
 	glutMouseFunc(mouse);
 	glutMotionFunc(mouseMotion);
 	glutIdleFunc(tampil);
-	
 	glutMainLoop();
 	return 0;
 }
@@ -149,16 +161,34 @@ void kubus(int x1,int y1,int z1,int p,int l,int t){
 		
 }
 
+//void jendela(int x1,int y1,int z,int p,int t){
+//  for(int i=0;i<11;i++){
+//  	int sp=0;
+//  	x1+=sp;
+//	int x2 = x1+p;
+//	int y2= y1;
+//	glBegin(GL_QUADS);
+//	glVertex3f(x1,y1,z);
+//	glVertex3f(x2,y2,z);
+//	glVertex3f(x2,y2+t,z);
+//	glVertex3f(x1,y1+t,z);
+//	glEnd();
+//	sp+=5;
+//	  }
+//	
+//	
+//		
+//}
 
 void init(void)
 {
-	glClearColor(0.812, 0.9450980392156863, 1.0, 0.0);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
 
 	//fungsi ke belakang gelap
-	glEnable(GL_LIGHTING);
-	glEnable(GL_COLOR_MATERIAL);
-	glEnable(GL_LIGHT0);
+//	glEnable(GL_LIGHTING);
+//	glEnable(GL_COLOR_MATERIAL);
+//	glEnable(GL_LIGHT0);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -169,25 +199,154 @@ void init(void)
 }
 
 void tampil(void)
-{
+{  float u = ((float)rand()/(float)(RAND_MAX)) * 1;
+    float p = ((float)rand()/(float)(RAND_MAX)) * 1;
+     float q = ((float)rand()/(float)(RAND_MAX)) * 1;
 	glPushMatrix();
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 	if (is_depth)
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClear(GL_COLOR_BUFFER_BIT);
-	//glLoadIdentity();
+//	glLoadIdentity();
 
 	//mengatur kamera
 	gluLookAt(0.0f, 5.0f, 50.0f,0.0f, 0.0f, 0.0f, 0.0f,  1.0f, 0.0f);
 	//biar bisa muter
 	glRotatef(xrot, 1.0f, 0.0f, 0.0f);
 	glRotatef(yrot, 0.0f, 1.0f, 0.0f);
-
-	glColor3f(1,1,1);
-	kubus(-50,-40,-20,90,100,70);
 	
+    //main building 1
+	glColor3f(0.95,0.40,0.20);
+	kubus(-400,-40,-20,500,100,40);
+	   //main sep 1
+	   glColor3f(0.95,0.9,0.85);
+   	   kubus(-400,0,-20,500,100,5);
+   	   
+   	//main building 2
+	glColor3f(0.95,0.40,0.20);
+	kubus(-400,5,-20,500,100,30);
+	    //main sep 2
+	   glColor3f(0.95,0.9,0.85);
+   	   kubus(-400,35,-20,500,100,5);
+   	   
+   	//main building 3
+	glColor3f(0.95,0.40,0.20);
+	kubus(-400,40,-20,500,100,30);
+	    //main sep 3
+	    glColor3f(0.95,0.9,0.85);
+   	   kubus(-400,70,-20,500,100,5);
+   	   
+				   	//mainside1
+				   	glColor3f(0.95,0.40,0.20);
+					kubus(0,75,-20,100,100,15);
+					   //mainside sep 1
+					   glColor3f(0.95,0.9,0.85);
+				   	   kubus(0,90,-20,100,100,1);
+				   	   
+				   	//mainside2
+				   	 glColor3f(0.95,0.40,0.20);
+					kubus(0,91,-20,100,100,4);
+					  //mainside sep 2
+					    glColor3f(0.95,0.9,0.85);
+				   	   kubus(0,95,-20,100,100,1);
+				   	   
+				   	   int yy=0;
+				   	   for(int j=0;j<=2;j++){
+				   	   	int sp=0;
+				   	   	for(int i=0;i<=11;i++){
+				   	   	glColor3f(0.95,0.9,0.85);
+				     	kubus(5+sp,-30+yy,-19,5,1.5,20);
+						sp+=8;  }
+						yy+=39;
+						  }
+				   	   
+				     	
+				     
+				     
+				   
+				   	
+	//mainbuilding 4
+   	glColor3f(0.95,0.40,0.20);
+	kubus(-400,75,-20,400,100,25);
+	 //main sep 4
+	    glColor3f(0.95,0.9,0.85);
+   	   kubus(-400,100,-20,400,100,5);
+   	   
+   	//mainbuilding 5
+     glColor3f(0.95,0.40,0.20);
+	kubus(-400,105,-20,400,100,30);
+	 //main sep 5
+	   glColor3f(0.95,0.9,0.85);
+   	   kubus(-400,135,-20,400,100,5);
+  	   
+   	     //roof1
+	     glColor3f(1,1,1);
+   	     kubus(-70,98,100,75,120,12);
+	   	     //pil1
+		     glColor3f(1,1,1);
+	   	     kubus(-5,-40,100,10,10,150);
+	   	     //pil2
+		     glColor3f(1,1,1);
+	   	     kubus(-5,-40,80,10,10,150);
+	   	     //-1
+	   	     glColor3f(1,1,1);
+	   	     kubus(-5,60,90,10,10,10);
+	   	      //-2
+	   	     glColor3f(1,1,1);
+	   	     kubus(-5,20,90,10,10,10);
+	   	     //pilb
+	   	     glColor3f(1,1,1);
+	   	       kubus(-70,-40,100,10,30,150);
+	   	     
+	   	     
+	   	     
+	   	     
+	    //roof2
+	     glColor3f(1,1,1);
+     	   kubus(-180,98,100,75,120,12);
+     	     //pila
+	   	     glColor3f(1,1,1);
+	   	       kubus(-115,-40,100,10,30,150);
+     	     //pilb
+	   	     glColor3f(1,1,1);
+	   	       kubus(-180,-40,100,10,30,150);
+   	   //rooffbawah
+	   glColor3f(1,1,1);
+	   kubus(-215,5,100,35,120,6);
+   	    //roof3
+	  glColor3f(1,1,1);
+   	   kubus(-290,98,100,75,120,12);
+   	   //pila
+	   	     glColor3f(1,1,1);
+	   	       kubus(-290,-40,100,10,30,150);
+     	     //pilb
+	   	     glColor3f(1,1,1);
+	   	       kubus(-225,-40,100,10,30,150);
+  	    //roof4
+	    glColor3f(1,1,1);
+  	   kubus(-400,98,100,75,120,12);
+  	   //pila
+	   	     glColor3f(1,1,1);
+	   	       kubus(-400,-40,100,10,30,150);
+     	     //pilb
+	   	     glColor3f(1,1,1);
+	   	       kubus(-335,-40,100,10,30,150);
+	
+   	   
+	
+		//tanah
+    
+    kubus(-400,-80,200,600,400,39.9);
+	
+	glRotatef(rot, 0, 1, 0);
+	
+	glColor3f(u,p,q);
+	kubus(20,150,20,60,20,39.9);
+
+
 	glPopMatrix();
+	
 	glutSwapBuffers();
 }
 
